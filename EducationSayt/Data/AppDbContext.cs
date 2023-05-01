f2e44aee82d6d3d28820733feea4a98e42d6b094
Alakbarbh <alakbarbh@code.edu.az>
2023-05-01 08:03:06 -0800

    One to many crud done

---
+++ EducationSayt/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EducationSayt.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationSayt.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseImage> CourseImages { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Newser> Newsers { get; set; }
+        public DbSet<New> News { get; set; }
+
+    }
+}
